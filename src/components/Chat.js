30d0077f275d449b3d0590c4460d2021c53c0d91
Syed M.Jafar Ali Naqvi <alimuhammadjafar873@gmail.com>
2023-12-15 23:59:03 +0500

    Chat module modified

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,186 +1,180 @@
 import React, { useEffect, useRef, useState } from "react";
 import {
-	doc,
-	setDoc,
-	getDoc,
-	updateDoc,
-	Timestamp,
-	collection,
-	query,
-	where,
-	onSnapshot,
+  doc,
+  setDoc,
+  getDoc,
+  updateDoc,
+  Timestamp,
+  collection,
+  query,
+  where,
+  onSnapshot,
 } from "firebase/firestore";
 import { db } from "@/firebase";
 
 const Chat = ({ senderID, receiverID }) => {
-	const [messages, setMessages] = useState([]);
-	const [newMessage, setNewMessage] = useState("");
-	const messagesBoxRef = useRef(null);
+  const [messages, setMessages] = useState([]);
+  const [newMessage, setNewMessage] = useState("");
+  const messagesBoxRef = useRef(null);
 
-	useEffect(() => {
-		const messageCollectionRef = collection(db, "Messages");
-		const combinedId1 = `${senderID}_${receiverID}`;
-		const combinedId2 = `${receiverID}_${senderID}`;
+  useEffect(() => {
+    const messageCollectionRef = collection(db, "Messages");
+    const combinedId1 = `${senderID}_${receiverID}`;
+    const combinedId2 = `${receiverID}_${senderID}`;
 
-		const unsubscribe = onSnapshot(
-			query(
-				messageCollectionRef,
-				where("__name__", "in", [combinedId1, combinedId2])
-			),
-			(querySnapshot) => {
-				const fetchedMessages = [];
-				querySnapshot.forEach((doc) => {
-					fetchedMessages.push(doc.data());
-				});
-				setMessages(fetchedMessages[0]?.messages);
-			}
-		);
+    const unsubscribe = onSnapshot(
+      query(
+        messageCollectionRef,
+        where("__name__", "in", [combinedId1, combinedId2])
+      ),
+      (querySnapshot) => {
+        const fetchedMessages = [];
+        querySnapshot.forEach((doc) => {
+          fetchedMessages.push(doc.data());
+        });
+        setMessages(fetchedMessages[0]?.messages);
+      }
+    );
 
-		return () => {
-			unsubscribe();
-		};
-	}, [senderID, receiverID]);
+    return () => {
+      unsubscribe();
+    };
+  }, [senderID, receiverID]);
 
-	const handleSendMessage = async () => {
-		if (newMessage.trim() !== "") {
-			try {
-				const timestamp = Timestamp.now();
-				const senderReceiverDocRef = doc(
-					db,
-					"Messages",
-					`${senderID}_${receiverID}`
-				);
-				const receiverSenderDocRef = doc(
-					db,
-					"Messages",
-					`${receiverID}_${senderID}`
-				);
+  const handleSendMessage = async () => {
+    if (newMessage.trim() !== "") {
+      try {
+        const timestamp = Timestamp.now();
+        const senderReceiverDocRef = doc(
+          db,
+          "Messages",
+          `${senderID}_${receiverID}`
+        );
+        const receiverSenderDocRef = doc(
+          db,
+          "Messages",
+          `${receiverID}_${senderID}`
+        );
 
-				const senderReceiverDocSnap = await getDoc(senderReceiverDocRef);
-				const receiverSenderDocSnap = await getDoc(receiverSenderDocRef);
+        const senderReceiverDocSnap = await getDoc(senderReceiverDocRef);
+        const receiverSenderDocSnap = await getDoc(receiverSenderDocRef);
 
-				let updatedMessages;
-				let selectedDocRef;
+        let updatedMessages;
+        let selectedDocRef;
 
-				if (senderReceiverDocSnap.exists()) {
-					updatedMessages = [
-						...(senderReceiverDocSnap.data().messages || []),
-						{
-							senderId: senderID,
-							text: newMessage,
-							timestamp: timestamp,
-						},
-					];
-					selectedDocRef = senderReceiverDocRef;
-				} else if (receiverSenderDocSnap.exists()) {
-					updatedMessages = [
-						...(receiverSenderDocSnap.data().messages || []),
-						{
-							senderId: senderID,
-							text: newMessage,
-							timestamp: timestamp,
-						},
-					];
-					selectedDocRef = receiverSenderDocRef;
-				} else {
-					updatedMessages = [
-						{
-							senderId: senderID,
-							text: newMessage,
-							timestamp: timestamp,
-						},
-					];
-					selectedDocRef = senderReceiverDocRef; // You can choose any of the two for a new document
-					await setDoc(selectedDocRef, { messages: updatedMessages });
-				}
+        if (senderReceiverDocSnap.exists()) {
+          updatedMessages = [
+            ...(senderReceiverDocSnap.data().messages || []),
+            {
+              senderId: senderID,
+              text: newMessage,
+              timestamp: timestamp,
+            },
+          ];
+          selectedDocRef = senderReceiverDocRef;
+        } else if (receiverSenderDocSnap.exists()) {
+          updatedMessages = [
+            ...(receiverSenderDocSnap.data().messages || []),
+            {
+              senderId: senderID,
+              text: newMessage,
+              timestamp: timestamp,
+            },
+          ];
+          selectedDocRef = receiverSenderDocRef;
+        } else {
+          updatedMessages = [
+            {
+              senderId: senderID,
+              text: newMessage,
+              timestamp: timestamp,
+            },
+          ];
+          selectedDocRef = senderReceiverDocRef; // You can choose any of the two for a new document
+          await setDoc(selectedDocRef, { messages: updatedMessages });
+        }
 
-				await updateDoc(selectedDocRef, { messages: updatedMessages });
-				setNewMessage("");
-				console.log("Message added successfully!");
-			} catch (error) {
-				console.error("Error adding message:", error);
-			}
-		}
-	};
+        await updateDoc(selectedDocRef, { messages: updatedMessages });
+        setNewMessage("");
+        console.log("Message added successfully!");
+      } catch (error) {
+        console.error("Error adding message:", error);
+      }
+    }
+  };
 
-	useEffect(() => {
-		scrollToBottom();
-	}, [messages]);
+  useEffect(() => {
+    scrollToBottom();
+  }, [messages]);
 
-	const scrollToBottom = () => {
-		if (messagesBoxRef.current) {
-			messagesBoxRef.current.scrollTop = messagesBoxRef.current.scrollHeight;
-		}
-	};
+  const scrollToBottom = () => {
+    if (messagesBoxRef.current) {
+      messagesBoxRef.current.scrollTop = messagesBoxRef.current.scrollHeight;
+    }
+  };
 
-	return (
-		<div className="flex-1 p-2 sm:p-6 justify-between flex flex-col h-screen">
-			<div
-				ref={messagesBoxRef}
-				className="bg-letter-grey rounded-sm border border-black-button"
-				style={{ height: "700px", overflowY: "auto" }}
-			>
-				{Array.isArray(messages) && messages.length > 0 ? (
-					messages.map((message, index) => (
-						<div
-							key={index}
-							className={`chat-message ${
-								message.senderId === senderID ? "items-end" : "items-start"
-							}`}
-						>
-							<div
-								className={`flex ${
-									message.senderId === senderID
-										? "justify-end"
-										: "justify-start"
-								}`}
-							>
-								<div
-									className={`flex flex-col space-y-2 text-xs max-w-xs mx-2 ${
-										message.senderId === senderID
-											? "order-1 items-end"
-											: "order-2 items-start"
-									}`}
-								>
-									<div>
-										<span
-											className={`mt-5 px-4 py-2 rounded-lg inline-block ${
-												message.senderId === senderID
-													? "rounded-br-none bg-gray-300 text-gray-600"
-													: "rounded-bl-none bg-white text-black"
-											}`}
-										>
-											{message.text}
-										</span>
-									</div>
-								</div>
-							</div>
-						</div>
-					))
-				) : (
-					<div className="flex justify-center items-center h-full">
-						<p className="text-gray-500">No messages yet</p>
-					</div>
-				)}
-			</div>
+  return (
+    <div className="flex-1 p-2 sm:p-6 justify-between flex flex-col h-screen">
+      <div
+        ref={messagesBoxRef}
+        className="bg-letter-grey rounded-sm border border-black-button"
+        style={{ height: "700px", overflowY: "auto" }}>
+        {Array.isArray(messages) && messages.length > 0 ? (
+          messages.map((message, index) => (
+            <div
+              key={index}
+              className={`chat-message ${
+                message.senderId === senderID ? "items-end" : "items-start"
+              }`}>
+              <div
+                className={`flex ${
+                  message.senderId === senderID
+                    ? "justify-end"
+                    : "justify-start"
+                }`}>
+                <div
+                  className={`flex flex-col space-y-2 text-xs max-w-xs mx-2 ${
+                    message.senderId === senderID
+                      ? "order-1 items-end"
+                      : "order-2 items-start"
+                  }`}>
+                  <div>
+                    <span
+                      className={`mt-5 px-4 py-2 rounded-lg inline-block ${
+                        message.senderId === senderID
+                          ? "rounded-br-none bg-gray-300 text-gray-600"
+                          : "rounded-bl-none bg-white text-black"
+                      }`}>
+                      {message.text}
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))
+        ) : (
+          <div className="flex justify-center items-center h-full">
+            <p className="text-gray-500">No messages yet</p>
+          </div>
+        )}
+      </div>
 
-			<div className="flex items-center space-x-2">
-				<input
-					className="text-letter-orange w-3/4 px-5 py-5 rounded-full"
-					type="text"
-					placeholder="Write your message!"
-					value={newMessage}
-					onChange={(e) => setNewMessage(e.target.value)}
-				/>
-				<button
-					className="bg-letter-orange bg-opacity-80 text-letter-grey border hover:bg-letter-grey hover:text-black-body font-bold py-2 px-16 rounded-full"
-					onClick={handleSendMessage}
-				>
-					Send
-				</button>
-			</div>
-		</div>
-	);
+      <div className="flex items-center space-x-2">
+        <input
+          className="text-letter-orange w-3/4 px-5 py-5 rounded-full"
+          type="text"
+          placeholder="Write your message!"
+          value={newMessage}
+          onChange={(e) => setNewMessage(e.target.value)}
+        />
+        <button
+          className="bg-letter-orange bg-opacity-80 text-letter-grey border hover:bg-letter-grey hover:text-black-body font-bold py-2 px-16 rounded-full"
+          onClick={handleSendMessage}>
+          Send
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Chat;
--- src/pages/FreelancerMessage/ListMessage/[id].js
+++ src/pages/FreelancerMessage/ListMessage/[id].js
@@ -16,235 +16,226 @@ import { RiLogoutCircleRLine } from "react-icons/ri";
 import Link from "next/link";
 
 const ListMessage = () => {
-	const [userIDS, setUserIDS] = useState([]);
-	const [userData, setUserData] = useState([]);
-	const messages = [
-		{
-			date: "2023-01-01",
-			user: "John Doe",
-			message: "Hello, how are you?",
-		},
-		// Add more messages as needed
-	];
-	const router = useRouter();
+  const [userIDS, setUserIDS] = useState([]);
+  const [messages, setMessage] = useState([]);
+  const [messagesData, setMessagesData] = useState([]);
+  const router = useRouter();
 
-	// const [message, setMessage] = useState("");
-	// Assuming you are using Next.js dynamic routing
-	useEffect(() => {
-		const { id } = router.query;
-		const fetchMessages = async () => {
-			try {
-				const messagesRef = collection(db, "Messages");
-				const querySnapshot = await getDocs(messagesRef);
+  // Assuming you are using Next.js dynamic routing
+  useEffect(() => {
+    const { id } = router.query;
+    const fetchMessages = async () => {
+      try {
+        const messagesRef = collection(db, "Messages");
+        const querySnapshot = await getDocs(messagesRef);
 
-				querySnapshot.forEach((doc) => {
-					// Access individual document data here
-					let messID = doc.id.split("_");
-					if (id == messID[0]) {
-						console.log("Document ID:", messID[1]);
-						setUserIDS((userIDS) => [...userIDS, messID[1]]);
-					} else if (id == messID[1]) {
-						setUserIDS((userIDS) => [...userIDS, messID[0]]);
-					}
-					console.log("Document data:", doc.data());
-					// Perform actions with each document's data as needed
-				});
-			} catch (error) {
-				console.error("Error fetching messages:", error);
-			}
-		};
+        querySnapshot.forEach((doc) => {
+          // Access individual document data here
+          let messID = doc.id.split("_");
+          if (id == messID[0]) {
+            setUserIDS((userIDS) => [...userIDS, messID[1]]);
+          } else if (id == messID[1]) {
+            setUserIDS((userIDS) => [...userIDS, messID[0]]);
+          }
+          // Perform actions with each document's data as needed
+          setMessagesData((messagesData) => [...messagesData, doc.data()]);
+        });
+      } catch (error) {
+        console.error("Error fetching messages:", error);
+      }
+    };
 
-		fetchMessages();
-		fetchDataFromFirestore();
-	}, [router.isReady, router.query]);
+    fetchMessages();
+  }, [router.isReady, router.query]);
 
-	useEffect(() => {
-		if (userIDS.length != 0) {
-			console.log(userData);
-		}
-	}, [userIDS]);
+  useEffect(() => {
+    if (userIDS.length != 0) {
+      fetchDataFromFirestore();
+    }
+  }, [userIDS]);
 
-	const [available, setAvailable] = useState(true);
+  const [available, setAvailable] = useState(true);
 
-	async function fetchDataFromFirestore() {
-		try {
-			console.log(userIDS)
-			for (const userId of userIDS) {
-				console.log('USER ID: '+userId);
-				const userDocRef = doc(db, "Professional", userId);
-				const userDocSnap = await getDoc(userDocRef);
+  async function fetchDataFromFirestore() {
+    try {
+      for (const userId of userIDS) {
+        const professionalDocRef = doc(db, "Professional", userId);
+        const freelancerDocRef = doc(db, "Freelancer", userId);
 
-				if (userDocSnap.exists()) {
-					const userData = userDocSnap.data();
-					setUserData((prevUserData) => [...prevUserData, userData]);
-				} else {
-					const freelancerDocRef = doc(db, "Freelancer", userId);
-					const freelancerDocSnap = await getDoc(freelancerDocRef);
-					if (freelancerDocSnap.exists()) {
-						const userData = freelancerDocSnap.data();
-						setUserData((prevUserData) => [...prevUserData, userData]);
-					} else {
-						console.log("User with this id not found!");
-					}
-				}
-			}
-			console.log('here');
-		} catch (error) {
-			console.error("Error fetching user data:", error);
-		}
-	}
+        const professionalDocSnap = await getDoc(professionalDocRef);
+        const freelancerDocSnap = await getDoc(freelancerDocRef);
 
-	return (
-		<div className="flex flex-col h-screen bg-black-body">
-			{/* Barre de navigation supérieure */}
+        if (professionalDocSnap.exists()) {
+          const messages = professionalDocSnap.data();
+          setMessage((prevUserData) => [...prevUserData, messages]);
+        } else if (freelancerDocSnap.exists()) {
+          const messages = freelancerDocSnap.data();
+          setMessage((prevUserData) => [...prevUserData, messages]);
+        } else {
+          console.log("User with id", userId, "not found!");
+        }
+      }
+    } catch (error) {
+      console.error("Error fetching user data:", error);
+    }
+  }
 
-			<div className="text-letter-orange">
-				<h1>Bienvenue userData.name, </h1>
-			</div>
-			<div className="bg-black-buttonshadow w-full p-2  flex items-center justify-between flex-row md:text-base md:hidden">
-				{/* ... */}
-				<nav className="flex items-center space-x-16 justify-around m-auto">
-					{/* <Link href={`/Message/{${id}`}>
+  return (
+    <div className="flex flex-col h-screen bg-black-body">
+      {/* Barre de navigation supérieure */}
+
+      <div className="text-letter-orange">
+        <h1>Bienvenue messages.name, </h1>
+      </div>
+      <div className="bg-black-buttonshadow w-full p-2  flex items-center justify-between flex-row md:text-base md:hidden">
+        {/* ... */}
+        <nav className="flex items-center space-x-16 justify-around m-auto">
+          {/* <Link href={`/Message/{${id}`}>
             <div className="items-center space-x-4 text-letter-grey">
               <PiEnvelopeThin className="text-letter-orange mt-1 mr-2 w-5 h-5" />
             </div>{" "}
           </Link> */}
 
-					<Link href="/FindMission">
-						<div className=" items-center space-x-4 text-letter-grey">
-							<FaMagnifyingGlass className="text-letter-orange mt-1 mr-2 w-5 h-5" />
-						</div>
-					</Link>
-					<Link href="/ProfilPro">
-						<div className=" items-center space-x-4 text-letter-grey">
-							<PiNewspaperClippingLight className="text-letter-orange mt-1 mr-2 w-5 h-5" />
-						</div>{" "}
-					</Link>
-					<Link href="/ProfilPro">
-						<div className=" items-center space-x-4 text-letter-grey">
-							<BsPersonWorkspace className="text-letter-orange mt-1 mr-2 w-5 h-5" />
-						</div>{" "}
-					</Link>
-					<Link href={"/CreateAnnonce/"}>
-						<div className=" items-center space-x-4 text-letter-grey">
-							<RiLogoutCircleRLine className="text-letter-orange mt-1 mr-2 w-5 h-5" />
-						</div>
-					</Link>
-				</nav>
-			</div>
+          <Link href="/FindMission">
+            <div className=" items-center space-x-4 text-letter-grey">
+              <FaMagnifyingGlass className="text-letter-orange mt-1 mr-2 w-5 h-5" />
+            </div>
+          </Link>
+          <Link href="/ProfilPro">
+            <div className=" items-center space-x-4 text-letter-grey">
+              <PiNewspaperClippingLight className="text-letter-orange mt-1 mr-2 w-5 h-5" />
+            </div>{" "}
+          </Link>
+          <Link href="/ProfilPro">
+            <div className=" items-center space-x-4 text-letter-grey">
+              <BsPersonWorkspace className="text-letter-orange mt-1 mr-2 w-5 h-5" />
+            </div>{" "}
+          </Link>
+          <Link href={"/CreateAnnonce/"}>
+            <div className=" items-center space-x-4 text-letter-grey">
+              <RiLogoutCircleRLine className="text-letter-orange mt-1 mr-2 w-5 h-5" />
+            </div>
+          </Link>
+        </nav>
+      </div>
 
-			{/* Contenu principal */}
-			<div className="flex-1 flex flex-wrap">
-				{/* Barre latérale de navigation */}
-				<div
-					className="p-2 bg-black-button w-full md:w-60 flex-col md:flex hidden"
-					id="sideNav"
-				>
-					<nav>
-						<Link href={""}>
-							<div className="flex ml-2 mt-9">
-								<div>
-									<PiEnvelopeThin className="text-letter-orange mt-1 mr-2 w-5 h-5" />
-								</div>
-								<div>
-									<ul className="flex items-center space-x-4 text-letter-grey">
-										{" "}
-										Message{" "}
-									</ul>
-								</div>
-							</div>{" "}
-						</Link>
-						<Link href={"/FindMission"}>
-							<div className="flex ml-2 mt-9">
-								<div>
-									<FaMagnifyingGlass className="text-letter-orange mt-1 mr-2" />
-								</div>
-								<div>
-									<ul className="flex items-center space-x-4 text-letter-grey">
-										{" "}
-										Trouver une mission{" "}
-									</ul>
-								</div>
-							</div>{" "}
-						</Link>
+      {/* Contenu principal */}
+      <div className="flex-1 flex flex-wrap">
+        {/* Barre latérale de navigation */}
+        <div
+          className="p-2 bg-black-button w-full md:w-60 flex-col md:flex hidden"
+          id="sideNav">
+          <nav>
+            <Link href={""}>
+              <div className="flex ml-2 mt-9">
+                <div>
+                  <PiEnvelopeThin className="text-letter-orange mt-1 mr-2 w-5 h-5" />
+                </div>
+                <div>
+                  <ul className="flex items-center space-x-4 text-letter-grey">
+                    {" "}
+                    Message{" "}
+                  </ul>
+                </div>
+              </div>{" "}
+            </Link>
+            <Link href={"/FindMission"}>
+              <div className="flex ml-2 mt-9">
+                <div>
+                  <FaMagnifyingGlass className="text-letter-orange mt-1 mr-2" />
+                </div>
+                <div>
+                  <ul className="flex items-center space-x-4 text-letter-grey">
+                    {" "}
+                    Trouver une mission{" "}
+                  </ul>
+                </div>
+              </div>{" "}
+            </Link>
 
-						<Link href="/ProfilPro">
-							<div className="flex ml-2 mt-9">
-								<div>
-									<PiNewspaperClippingLight className="text-letter-orange mt-1 mr-2" />
-								</div>
-								<div>
-									<ul className="flex items-center space-x-4 text-letter-grey">
-										{" "}
-										Suivre mes candidatures{" "}
-									</ul>
-								</div>
-							</div>{" "}
-						</Link>
+            <Link href="/ProfilPro">
+              <div className="flex ml-2 mt-9">
+                <div>
+                  <PiNewspaperClippingLight className="text-letter-orange mt-1 mr-2" />
+                </div>
+                <div>
+                  <ul className="flex items-center space-x-4 text-letter-grey">
+                    {" "}
+                    Suivre mes candidatures{" "}
+                  </ul>
+                </div>
+              </div>{" "}
+            </Link>
 
-						<Link href="/ProfilPro">
-							<div className="flex ml-2 my-10">
-								<div>
-									<RiLogoutCircleRLine className="text-letter-orange mt-1 mr-2" />
-								</div>
+            <Link href="/ProfilPro">
+              <div className="flex ml-2 my-10">
+                <div>
+                  <RiLogoutCircleRLine className="text-letter-orange mt-1 mr-2" />
+                </div>
 
-								<div>
-									<ul className="flex items-center space-x-4 text-letter-grey">
-										{" "}
-										Déconnecter{" "}
-									</ul>
-								</div>
-							</div>{" "}
-						</Link>
-					</nav>
+                <div>
+                  <ul className="flex items-center space-x-4 text-letter-grey">
+                    {" "}
+                    Déconnecter{" "}
+                  </ul>
+                </div>
+              </div>{" "}
+            </Link>
+          </nav>
 
-					{/* ... */}
-				</div>
+          {/* ... */}
+        </div>
 
-				{/* Zone de contenu principal */}
-				<div className=" text-letter-grey flex-1 p-4 w-full ">
-					{/* Champ de recherche */}
-					<div className="space-y-5">
-						<div className="border-b border-b-gray-200">
-							<ul className="-mb-px flex items-center gap-4 text-sm font-medium">
-								{/* Add more menu items as needed */}
-							</ul>
-						</div>
-						<div>
-							{/* Render messages as a table */}
-							<table className=" text-letter-orange w-full border border-gray-200 rounded-xl overflow-hidden">
-								<thead className="bg-black-button">
-									<tr>
-										<th className="py-2 px-4 border-b">Date</th>
-										<th className="py-2 px-4 border-b">User</th>
-										<th className="py-2 px-4 border-b">Message</th>
-									</tr>
-								</thead>
-								<tbody>
-									{messages.map((msg, index) => (
-										<tr
-											key={index}
-											className={index % 2 === 0 ? "bg-gray-100" : ""}
-										>
-											<td className=" text-center py-2 px-4 border-b">
-												{msg.email}
-											</td>
-											<td className=" text-center py-2 px-4 border-b">
-												{'Kaleem'}
-											</td>
-											<td className=" text-center py-2 px-4 border-b">
-												{'test'}
-											</td>
-										</tr>
-									))}
-								</tbody>
-							</table>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+        {/* Zone de contenu principal */}
+        <div className=" text-letter-grey flex-1 p-4 w-full ">
+          {/* Champ de recherche */}
+          <div className="space-y-5">
+            <div className="border-b border-b-gray-200">
+              <ul className="-mb-px flex items-center gap-4 text-sm font-medium">
+                {/* Add more menu items as needed */}
+              </ul>
+            </div>
+            <div>
+              {/* Render messages as a table */}
+              <table className=" text-letter-orange w-full border border-gray-200 rounded-xl overflow-hidden">
+                <thead className="bg-black-button">
+                  <tr>
+                    <th className="py-2 px-4 border-b">Date</th>
+                    <th className="py-2 px-4 border-b">User</th>
+                    <th className="py-2 px-4 border-b">Message</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {messages.map((msg, index) => (
+                    <tr
+                      key={index}
+                      onClick={() => router.push(`/FreelancerMessage/ChatMessage/${userIDS[index]}`)}
+                      className={`cursor-pointer ${
+                        index % 2 === 0 ? "bg-gray-100" : ""
+                      }`}>
+                      <td className=" text-center py-2 px-4 border-b">
+                        {msg.email}
+                      </td>
+                      <td className=" text-center py-2 px-4 border-b">
+                        {msg.type}
+                      </td>
+                      <td className=" text-center py-2 px-4 border-b">
+                        {
+                          messagesData[index].messages[
+                            messagesData[index].messages.length - 1
+                          ].text
+                        }
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ListMessage;
